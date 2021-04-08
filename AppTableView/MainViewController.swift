1e0e225b4333571efa8b9ec4ecadb705c3ad4667
olehdovhan <81869684+olehdovhan@users.noreply.github.com>
2021-04-08 22:32:47 +0300

    Solution - convert jpg to png

--- AppTableView/MainViewController.swift
+++ AppTableView/MainViewController.swift
@@ -9,44 +9,39 @@ import UIKit
 
 class MainViewController: UITableViewController {
     
-    let restarauntNames = [
-        "Burger Heroes", "Kitchen", "Bonsai", "Дастархан",
+    let restaurantNames = ["Burger Heroes", "Kitchen", "Bonsai", "Дастархан",
         "Индокитай", "X.O", "Балкан Гриль", "Sherlock Holmes",
         "Speak Easy", "Morris Pub", "Вкусные истории",
         "Классик", "Love&Life", "Шок", "Бочка"
     ]
-    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-       
+        
     }
+
+    // MARK: - Table view data source
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-       
-        return restarauntNames.count
+        return restaurantNames.count
     }
+    
 
- 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
 
-        cell.textLabel?.text = restarauntNames[indexPath.row]
-        cell.imageView?.image = UIImage(named: restarauntNames[indexPath.row])
+        cell.textLabel?.text = restaurantNames[indexPath.row]
+        cell.imageView?.image = UIImage(named: restaurantNames[indexPath.row])
+        cell.imageView?.layer.cornerRadius = cell.frame.size.height / 2
+        cell.imageView?.clipsToBounds = true
 
         return cell
     }
     
-
-   
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Table view delegate
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 85
     }
-   
-
 }
