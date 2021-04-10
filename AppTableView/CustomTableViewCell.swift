a9218c89dde8eebc637ea8f905d0af4109c5ec9d
olehdovhan <81869684+olehdovhan@users.noreply.github.com>
2021-04-10 12:20:31 +0300

    custom table view cell

---
+++ AppTableView/CustomTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CustomTableViewCell.swift
+//  AppTableView
+//
+//  Created by Oleh Dovhan on 10.04.2021.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+
+    @IBOutlet weak var locationLabel: UILabel!
+    
+    @IBOutlet weak var typeLabel: UILabel!
+    
+
+}
